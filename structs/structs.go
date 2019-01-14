e9f4868a8cec7d17c9fd636b492304814a7d11f2
Fabian Süß <despia@users.noreply.github.com>
2019-01-14 12:13:35 +0100

    extended config

--- structs/structs.go
+++ structs/structs.go
@@ -2,6 +2,10 @@ package structs
 
 // Config is the structure corresponding to config.yaml that holds all static configuration data
 type Config struct {
-	URLAuthenticationService string `yaml:"url_authentication_service"`
-	URLUserManagementService string `yaml:"url_user_management_service"`
+	URLAuthenticationService string `yaml:"authentication"`
+	AuthenticationLogin      string `yaml:"authentication_login"`
+	AuthenticationSignup     string `yaml:"authentication_signup"`
+	URLUserManagementService string `yaml:"user_management"`
+	UserManagementLogin      string `yaml:"user_management_login"`
+	UserManagementSignup     string `yaml:"user_management_signup"`
 }
