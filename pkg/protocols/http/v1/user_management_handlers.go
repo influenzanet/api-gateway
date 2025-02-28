363ab5d2e46e9fdd687cc5cf2af43eb41cd51c68
andrea.casaluci <casaluci10@gmail.com>
2025-02-28 09:50:54 +0100

    chore: change addphonenumber input object

--- pkg/protocols/http/v1/user_management_handlers.go
+++ pkg/protocols/http/v1/user_management_handlers.go
@@ -135,6 +135,19 @@ func (h *HttpEndpoints) signupWithEmailHandlV3(c *gin.Context) {
 	)
 }
 
+func (h *HttpEndpoints) addPhoneNumber(c *gin.Context) {
+	h.grpcCallHandler(
+		c,
+		func(c *gin.Context) (protoreflect.ProtoMessage, error) {
+			var req umAPI.ContactInfoMsg
+			if err := h.JsonToProto(c, &req); err != nil {
+				return nil, status.Error(codes.InvalidArgument, err.Error())
+			}
+			return h.clients.UserManagement.AddPhoneNumber(context.Background(), &req)
+		},
+	)
+}
+
 func (h *HttpEndpoints) grpcCallHandler(c *gin.Context, customMethod customHandlerMethod) {
 	resp, err := customMethod(c)
 	h.handleGRPCResponse(c, resp, err)
